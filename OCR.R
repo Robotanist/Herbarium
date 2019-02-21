91f9353ed42d49af94e02728a4512787cf12c915
Solomon Champion <solomon2@hawaii.edu>
2019-02-20 14:22:32 -1000

    Update OCR.R

--- OCR.R
+++ OCR.R
@@ -3,19 +3,10 @@ library("readr")
 
 
 OCR <- read_csv("/Users/solomon.champion/R/rows.csv",col_names = FALSE)
-View(OCR)
+#View(OCR)
 
-# Transpose col to rows
-# tdt <- t(OCR)
-
-# number of characters in each record
-# nchar(OCR)
-
-# average number characters per record 
-# mean(nchar(OCR[1:100]))
-
-# extracts HAW catalogue number from first 8 characters of record in CSV
-# named <- substr(tdt, 0, 8)
+# Transpose col to rows if needed
+# tOCR <- t(OCR)
 
 # extract HAW catalogue number from each record "HAW******.JPEG"
 
@@ -30,3 +21,19 @@ HAW_num
 mergecsv <- cbind(HAW_num,OCR)
 
 mergecsv
+
+# Removes blank first row if needed
+# mergecsv = mergecsv[-1,]
+
+# Suppress blank column if needed
+mergecsv$X1 = NULL
+
+# number of characters in each record
+nchar(mergecsv$X2)
+
+# average number characters per record 
+# mean(nchar(mergecsv$X2))
+
+# Rename column
+colnames(mergecsv)[colnames(mergecsv)=="X2"] <- "Descripiton"
+
